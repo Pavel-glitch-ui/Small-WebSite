53e8b5b29e375c2538bdeef4511c13cbb4a00b42
Pavel-glitch-ui <pavelsatete@mail.ru>
2025-08-17 20:38:32 +0900

    Delete login directory

--- login/layout.tsx
+++
@@ -1,8 +0,0 @@
-import NextAuthProvider from "../NextAuthProvider" 
-export default function LoginLayout({ children }: { children: React.ReactNode }){
-    return(
-            <NextAuthProvider>
-            {children}
-            </NextAuthProvider>
-    )
-}
